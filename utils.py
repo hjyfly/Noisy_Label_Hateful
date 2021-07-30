11382fcdc68c3386a5b7ff249c0ee73211e19ff6
hjyfly <hjyfly@gmail.com>
2021-07-30 09:30:44 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -136,28 +136,12 @@ class CustomTrainer(Trainer):
 
       return clean_inputs
 
-    def custom_training_step(self, model: nn.Module, inputs: Dict[str, Union[torch.Tensor, Any]], cur_epoch=-1) -> torch.Tensor:
-        """
-        Perform a training step on a batch of inputs.
-
-        Subclass and override to inject custom behavior.
-
-        Args:
-            model (:obj:`nn.Module`):
-                The model to train.
-            inputs (:obj:`Dict[str, Union[torch.Tensor, Any]]`):
-                The inputs and targets of the model.
-
-                The dictionary will be unpacked before being fed to the model. Most models expect the targets under the
-                argument :obj:`labels`. Check your model's documentation for all accepted arguments.
-
-        Return:
-            :obj:`torch.Tensor`: The tensor with training loss on this batch.
-        """
+    def custom_training_step(self, model: nn.Module, inputs: Dict[str, Union[torch.Tensor, Any]],
+                             cur_epoch=-1) -> torch.Tensor:
         model.train()
         inputs = self._prepare_inputs(inputs)
 
-        if self.args.p_threshold > 0 and cur_epoch > 2:
+        if self.args.p_threshold > 0 and cur_epoch >= 2:
           inputs = self.get_clean(model, inputs)
 
         loss = self.compute_loss(model, inputs)
